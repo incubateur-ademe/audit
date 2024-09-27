06f29b02dcddcf4a5abce45e7f2931820df376b6
Olivier Laurendeau <o.laurendeau@gmail.com>
2024-09-27 20:34:37 +0200

    fix(matomo): fix env vars injection

--- src/infrastructure/matomo.ts
+++ src/infrastructure/matomo.ts
@@ -1,8 +1,8 @@
 import { init, push } from "@socialgouv/matomo-next";
 
 // TODO: Replace with  Matomo URL and site ID when available
-const MATOMO_URL = process.env.MATOMO_URL as string;
-const MATOMO_SITE_ID = process.env.MATOMO_SITE_ID as string;
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL as string;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID as string;
 const IS_DEV = process.env.NODE_ENV !== "production";
 
 export namespace MatomoService {
